be0894db47332664b68b00a75639980041ff2148
Gabriel Groppo <gabgroppo@gmail.com>
2026-04-02 14:25:43 -0300

    Add files via upload

---
+++ hive/TelaPerfil.swift
@@ -0,0 +1,18 @@
+//
+//  TelaPerfil.swift
+//  hive
+//
+//  Created by GABRIEL GROPPO on 02/04/26.
+//
+
+import SwiftUI
+
+struct TelaPerfil: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TelaPerfil()
+}
---
+++ hive/TelaPix.swift
@@ -0,0 +1,18 @@
+//
+//  TelaPix.swift
+//  hive
+//
+//  Created by GABRIEL GROPPO on 02/04/26.
+//
+
+import SwiftUI
+
+struct TelaPix: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TelaPix()
+}
